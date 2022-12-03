f0e2d38174009e9765d42e17e29c833e916a5091
franiuz <francogrimoldidelgado@gmail.com>
2022-12-03 13:00:31 -0300

    primer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
 
 
 function App() {
   return (
-    <NavBar/>
+    <NavBar />
   );
 }
 
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -2,7 +2,30 @@ import React from "react";
 
 export const NavBar = () => {
     return (
-        <h1>Navbar</h1>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+          <a class="navbar-brand" href="#">COFFEE STORE</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Limpieza</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Maquinaria</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled">Elementos</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     )
 }
 
